38f72d43c48b7f96e4b36e92ead8bb51f43123cb
lichao0817 <cli94@wisc.edu>
2015-05-21 14:18:41 -0400

    remove unused variables

--- app/src/main/java/com/airwatch/docviewer/DocViewerFragment.java
+++ app/src/main/java/com/airwatch/docviewer/DocViewerFragment.java
@@ -14,50 +14,15 @@ import android.view.ViewGroup;
  * Activities that contain this fragment must implement the
  * {@link DocViewerFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link DocViewerFragment#newInstance} factory method to
+ * Use the {@link DocViewerFragment} factory method to
  * create an instance of this fragment.
  */
 public class DocViewerFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DocViewerFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DocViewerFragment newInstance(String param1, String param2) {
-        DocViewerFragment fragment = new DocViewerFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     public DocViewerFragment() {
         // Required empty public constructor
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
